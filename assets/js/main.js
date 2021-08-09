1a7ffafc071a6164de5267ebd9b3b070431e019a
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-08-09 15:56:09 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION ===*/
+/*==== SCROLL REVEAL ANIMATION ===*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
