b62ab39382eaa5376076a5c144e18f330ee6c5a4
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-08-09 15:55:48 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -25,7 +25,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+/*===== SCROLL REVEAL ANIMATION ===*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
